a1d0a05ccafc3a67b25e91edb7d8a65101ad58e9
Ivan Kirichenko <ivan.kirichenko@zalando.de>
2017-10-18 16:27:41 +0200

    opentracing-poc versions of dependencies and some logging enabled

--- src/main/java/de/zalando/zmon/dataservice/config/InstanaConfiguration.java
+++ src/main/java/de/zalando/zmon/dataservice/config/InstanaConfiguration.java
@@ -2,6 +2,7 @@ package de.zalando.zmon.dataservice.config;
 
 import com.instana.opentracing.InstanaTracer;
 import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
 import io.opentracing.util.ThreadLocalActiveSpanSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 public class InstanaConfiguration {
     @Bean
     public Tracer instanaTracer() {
-        return new InstanaTracer(new ThreadLocalActiveSpanSource());
+        Tracer tracer = new InstanaTracer(new ThreadLocalActiveSpanSource());
+        GlobalTracer.register(tracer);
+        return tracer;
     }
 }
